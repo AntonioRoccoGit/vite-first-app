23e49978732fc6054d147f7015d8d6445d7302a6
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-04-12 12:38:30 +0200

    added first component

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppBanner",
+    data() {
+        return {
+            string: "Questo è il mio primo componente con vue"
+        }
+    }
+}
+</script>
+<template>
+    <h2>Banner</h2>
+    <p>{{ string }}</p>
+</template>
+
+<style></style>
